f668473072da4b9bebbf422e66854b48b0e07096
Nirali D Bafna <75671090+niralidbafna@users.noreply.github.com>
2022-06-16 22:18:36 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,16 +8,136 @@ float bg_r = 0, bg_g = 0.8, bg_b = 1.5; //bg down
 float bg_r2 = 1, bg_g2 = 1, bg_b2 = 0; //bg up
 float star_r = 1, star_g = 1, star_b = 0;
 float school_dr = 184, school_dg = 88, school_db = 64;
-float grass_g = 0.5;
+float grass_g = 0.5, ps=1;
+GLint xr = 0, yr = 0;
+GLint cx1=115, cy1=110, cx4=115, cy4=130;//carlight co-ordinates
 struct Point {
 	GLint x;
 	GLint y;
 };
+float  counter = 600;
 void init(void)
 {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 	gluOrtho2D(0, 1000, 0, 600);
+	glMatrixMode(GL_MODELVIEW);
+}
+void wheel(int x, int y)
+{
+	float th;
+	glBegin(GL_POLYGON);
+	glColor3f(0, 0, 0);
+	for (int i = 0; i < 360; i++)
+	{
+		th = i * (3.1416 / 180);
+		glVertex2f(x + 18 * cos(th), y + 18 * sin(th));
+	}
+
+	glEnd();
+
+	glPolygonMode(GL_FRONT, GL_LINE);
+	glBegin(GL_POLYGON);
+	glColor3f(1, 1, 1);
+	for (int i = 0; i < 360; i++)
+	{
+		th = i * (3.1416 / 180);
+		glVertex2f(x + 18 * cos(th), y + 18 * sin(th));
+	}
+
+	glEnd();
+
+	glPolygonMode(GL_FRONT, GL_FILL);
+
+}
+void moon(int x, int y)
+{
+	float th;
+	glBegin(GL_POLYGON);
+	glColor3f(1,1,1);
+	for (int i = 0; i < 360; i++)
+	{
+		th = i * (3.1416 / 180);
+		glVertex2f(x + xr * cos(th), y + yr * sin(th));
+	}
+
+	glEnd();
+}
+void carlight()
+{
+	glColor3f(light_r, light_g, light_b);
+	glBegin(GL_POLYGON);
+	glVertex2i(cx1, cy1);
+	glVertex2i(120,115);
+	glVertex2i(120,125);
+	glVertex2i(cx4,cy4);
+	glEnd();
+}
+void car()
+{
+	//Bottom Part
+	glLoadIdentity();
+	counter = counter - 0.05;
+	glColor3ub(37, 64, 154);
+	glTranslated(counter, -60, 0.0);
+	glBegin(GL_POLYGON);
+	glVertex2f(120, 100);
+	glVertex2f(370, 100);
+	glVertex2f(370, 140);
+	glVertex2f(120, 140);
+
+	//Top Part
+	glBegin(GL_POLYGON);
+	glVertex2f(150, 140);
+	glVertex2f(350, 140);
+	glVertex2f(300, 180);
+	glVertex2f(200, 180);
+	glEnd();
+
+	//window1
+	glColor3f(1, 1, 1);
+	glBegin(GL_POLYGON);
+	glVertex2i(175,145);
+	glVertex2i(240,145);
+	glVertex2i(240,172);
+	glVertex2i(202,172);
+	glEnd();
+	
+
+	carlight();
+
+	//window2
+	glColor3f(1, 1, 1);
+	glBegin(GL_POLYGON);
+	glVertex2i(255, 145);
+	glVertex2i(333, 145);
+	glVertex2i(300, 172);
+	glVertex2i(255, 172);
+	glEnd();
+
+	//door
+	glColor3f(0, 0, 0);
+	glBegin(GL_LINES);
+	glVertex2i(247, 105);
+	glVertex2i(247, 175);
+	glEnd();
+
+
+
+	wheel(190, 100);
+	wheel(305, 100);
+
+	glColor3f(1, 1, 1);
+	glBegin(GL_POINTS);
+	glVertex2i(190, 100);
+	glVertex2i(305, 100);
+	glEnd();
+
+	
+
+
+
 }
 void print(int x, int y, float r, float g, float b, const char* str)
 {
@@ -101,7 +221,7 @@ void school()
 	glVertex2f(680, 350);
 	glVertex2f(320, 350);
 	glEnd();
-	
+
 
 	//board
 	glBegin(GL_POLYGON);
@@ -111,7 +231,7 @@ void school()
 	glVertex2f(590, 380);
 	glVertex2f(420, 380);
 	glEnd();
-	print(430,345,255,255,255,"RNS SCHOOL");
+	print(430, 345, 255, 255, 255, "RNS SCHOOL");
 
 	//board border
 	glBegin(GL_LINE_LOOP);
@@ -162,7 +282,7 @@ void school()
 	for (int j = 0; j < 4; j++) {
 		glBegin(GL_POLYGON);
 		glColor3ub(59, 91, 132);
-		glVertex2f(p , q);
+		glVertex2f(p, q);
 		glVertex2f(p + 50, q);
 		glVertex2f(p + 50, q + 50);
 		glColor3ub(97, 131, 159);
@@ -226,10 +346,10 @@ void theatre()
 
 	//inner bottom
 	glBegin(GL_POLYGON);
-	glVertex2f(40,140);
-	glVertex2f(280,140);
-	glVertex2f(280,230);
-	glVertex2f(40,230);
+	glVertex2f(40, 140);
+	glVertex2f(280, 140);
+	glVertex2f(280, 230);
+	glVertex2f(40, 230);
 	glEnd();
 
 	// door
@@ -269,8 +389,8 @@ void theatre()
 	glColor3f(0, 0, 0);
 	glLineWidth(5);
 	glBegin(GL_LINES);
-	glVertex2i(45,260);
-	glVertex2i(45,280);
+	glVertex2i(45, 260);
+	glVertex2i(45, 280);
 	glVertex2i(275, 260);
 	glVertex2i(275, 280);
 	glEnd();
@@ -278,10 +398,10 @@ void theatre()
 
 	//top
 	glBegin(GL_POLYGON);
-	glVertex2f(10,280);
-	glVertex2f(310,280);
-	glVertex2f(310,330);
-	glVertex2f(10,330);
+	glVertex2f(10, 280);
+	glVertex2f(310, 280);
+	glVertex2f(310, 330);
+	glVertex2f(10, 330);
 	glEnd();
 
 	//THEATRE
@@ -304,7 +424,7 @@ void theatre()
 		glVertex2i(p, q);
 		q += 12;
 	}
-	
+
 	//top
 	p = 10; q = 330;
 	for (int i = 0; i < 26; i++) {
@@ -484,7 +604,7 @@ void stars()
 		srand(i + 100);
 		x = rand() % 1000;
 		y = rand() % 600 + 460;
-		
+		glPointSize(ps);
 		glBegin(GL_POINTS);
 		glVertex2i(x, y);
 		glEnd();
@@ -550,7 +670,7 @@ void road()
 void grass()
 {
 	glColor3f(0, grass_g, 0);
-	
+
 	glBegin(GL_POLYGON);
 	glVertex2i(0, 80);
 	glVertex2i(1000, 80);
@@ -559,18 +679,30 @@ void grass()
 	glEnd();
 	glFlush();
 }
+void cardisplay()
+{
+	//Push and pop matrix for separating circle object from Background
+	glPushMatrix();
+	car();
+	glutSwapBuffers();
+	glPopMatrix();
+	glFlush();
+	glutPostRedisplay();
+}
 void scene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	background();
 	stars();
+	moon(850, 540);
 	road();
 	grass();
 	school();
 	house();
 	theatre();
 	streetlight();
+	cardisplay();
 }
 void keyboard(unsigned char key, int x, int y)
 {
@@ -581,8 +713,9 @@ void keyboard(unsigned char key, int x, int y)
 		grass_g = 0.2;
 		bg_r2 = 0; bg_g2 = 0; bg_b2 = 0;
 		school_dr = 200; school_dg = 88; school_db = 64;
-
-		glPointSize(2);
+		cx1 = 90; cy1 = 100; cx4 = 90; cy4 = 140;
+		ps = 2; //point size
+		xr = 27; yr = 27; //moon x_radius, y_radius
 	}
 	else if (key == 'd') {
 		bg_r = 0; bg_g = 0.8; bg_b = 1.5; //bg down
@@ -591,8 +724,9 @@ void keyboard(unsigned char key, int x, int y)
 		grass_g = 0.5;
 		bg_r2 = 1; bg_g2 = 1; bg_b2 = 0; //bg up
 		school_dr = 184; school_dg = 88; school_db = 64;
-
-		glPointSize(1);
+		cx1 = 115; cy1 = 110; cx4 = 115; cy4 = 130;
+		ps = 1;
+		xr = 0.1; yr = 0.1;
 
 	}
 	glutPostRedisplay();
@@ -601,13 +735,15 @@ void keyboard(unsigned char key, int x, int y)
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowPosition(100, 0);
 	glutInitWindowSize(1000, 600);
 	glutCreateWindow("CITY VIEW");
+	
+
+	init();
 	glutDisplayFunc(scene);
 	glutKeyboardFunc(keyboard);
-	init();
 	glutMainLoop();
 	return 0;
 }
